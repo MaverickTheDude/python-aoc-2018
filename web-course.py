6a10cabaf75fa515fe3b5b01be4dc1e96f9dec69
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2020-05-07 21:22:46 +0200

    Zadanie domowe: kurs 4, tydz. 2
    Prosta baza danych:
    znajdz wszystkie domeny mailowe i zlicz liczbe ich wystapien (jak slownik)

--- web-course.py
+++ web-course.py
@@ -1,51 +1,35 @@
-import urllib.request, urllib.parse, urllib.error
-import json
-import ssl
+import sqlite3
+import re
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
+conn = sqlite3.connect('zad1.sqlite')
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+fh = open('text.txt')
+it = 0
+for line in fh:
+    if not line.startswith('From: '): continue
+    org = re.findall('@(.+)\s', line)
+    org = org[0]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    it += 1
+    if it % 100 == 0:
+        conn.commit()
+conn.commit()
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
 
-address = 'Drexel'
-
-parms = dict()
-parms['address'] = address
-if api_key is not False: parms['key'] = api_key
-url = serviceurl + urllib.parse.urlencode(parms)
-
-print('Retrieving', url)
-uh = urllib.request.urlopen(url, context=ctx)
-data = uh.read().decode()
-print('Retrieved', len(data), 'characters')
-
-try:
-    js = json.loads(data)
-except:
-    js = None
-
-if not js or 'status' not in js or js['status'] != 'OK':
-    print('==== Failure To Retrieve ====')
-    print(data)
-
-print(json.dumps(js, indent=4))
-
-print(js['results'][0].keys())
-print(js['results'][0]['place_id'])
-# lat = js['results'][0]['geometry']['location']['lat']
-# lng = js['results'][0]['geometry']['location']['lng']
-# print('lat', lat, 'lng', lng)
-# location = js['results'][0]['formatted_address']
-# print(location)
+cur.close()
