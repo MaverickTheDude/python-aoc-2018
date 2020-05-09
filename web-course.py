313d10c5f8d88a83b75493fbe4dc0be011222862
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2020-05-10 01:14:03 +0200

    Zadanie domowe: kurs 4, tydzien 3
    Baza danych rejestrujaca atrybuty trackow muzycznych
    (one to one relations)

--- web-course.py
+++ web-course.py
@@ -1,35 +1,105 @@
+# Post na forum:
+# Looks like there is an unfortunate name clash with some of the Album names, e.g. both Queen and Frank Sinatra have an album named Greatest Hits. Provided code seems not to take it into account, as Frank Sinatra, whose id=23, doesn't appear in the column artist_id of Album table. 
+# A simple example will expose this. The following SQL code returns track by Frank Sinatra, however, the DB thinks it's by Queen.
+# 
+# Cheers!
+# 
+# SELECT Track.title, Artist.name, Album.title, Genre.name 
+#     FROM Track JOIN Genre JOIN Album JOIN Artist 
+#     ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+#     AND Album.artist_id = Artist.id 
+# 	WHERE Track.title = 'It was a Very Good Year'
+
+import xml.etree.ElementTree as ET
 import sqlite3
-import re
 
 conn = sqlite3.connect('zad1.sqlite')
 cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS Counts')
-cur.execute('''
-CREATE TABLE Counts (org TEXT, count INTEGER)''')
+fname = 'text.xml'
 
-fh = open('text.txt')
-it = 0
-for line in fh:
-    if not line.startswith('From: '): continue
-    org = re.findall('@(.+)\s', line)
-    org = org[0]
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
-    row = cur.fetchone()
-    if row is None:
-        cur.execute('''INSERT INTO Counts (org, count)
-                VALUES (?, 1)''', (org,))
-    else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
-                    (org,))
-    it += 1
-    if it % 100 == 0:
-        conn.commit()
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS Genre;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+''')
+
+
+# <key>Track ID</key><integer>369</integer>
+# <key>Name</key><string>Another One Bites The Dust</string>
+# <key>Artist</key><string>Queen</string>
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found: return child.text
+        if child.tag == 'key' and child.text == key:
+            found = True
+    return None
+
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print('Dict count:', len(all))
+for entry in all:
+    if (lookup(entry, 'Track ID') is None): continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    genre = lookup(entry, 'Genre')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    if name is None or artist is None or album is None or genre is None:
+        continue
+
+    #print(name, artist, album, count, rating, length)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', (artist,))
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist,))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', (album, artist_id))
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album,))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', (genre,))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre,))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+                (name, album_id, genre_id, length, rating, count))
+
 conn.commit()
-
-# https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
-
-for row in cur.execute(sqlstr):
-    print(str(row[0]), row[1])
-
 cur.close()
